7b06f02aaf5dc2f5dcc1fa85a5c1aa56a02138d4
Rudransh Shrivastava <rudransh.shr@gmail.com>
2025-06-26 08:52:36 +0530

    fix: add relative linenumbers and fix the sign column

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,14 @@
 require("psyduck")
 require("config.lazy")
 
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "yes"
 vim.diagnostic.config({
   virtual_text = {
     prefix = "■",
     spacing = 2,
   },
   update_in_insert = true,
-  signs = false,
 })
