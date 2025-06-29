864aa48ad67df31b30538009b6ebcbeb9ec68db7
Rudransh Shrivastava <rudransh.shr@gmail.com>
2025-06-29 12:05:12 +0530

    feat: a lot of additions from kickstart.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,17 +1,60 @@
 require("psyduck")
 require("config.lazy")
 
+-- Configure tabs
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.softtabstop = 4
+
+-- Enable relative line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+-- Enable the sign column
 vim.opt.signcolumn = "yes"
+
+-- Prettier diagnostics
 vim.diagnostic.config({
-  virtual_text = false,
-  virtual_lines = {
-    prefix = "■",
-    spacing = 2,
-  }
+    virtual_text = false,
+    virtual_lines = {
+        prefix = "■",
+        spacing = 2,
+    }
 })
+
+-- Enable mouse mode
+vim.o.mouse = 'a'
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Save undo history
+vim.o.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default
+vim.o.signcolumn = 'yes'
+
+-- Decrease update time
+vim.o.updatetime = 250
+
+-- Decrease mapped sequence wait time
+vim.o.timeoutlen = 300
+
+-- Preview substitutions as you type
+vim.o.inccommand = 'split'
+
+-- Show which line your cursor is on
+vim.o.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.o.scrolloff = 10
+
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+vim.o.confirm = true
