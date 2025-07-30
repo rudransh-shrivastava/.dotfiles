7de18cf341da5e248108a53cd774056b398c6933
Rudransh Shrivastava <rudransh.shr@gmail.com>
2025-07-30 20:29:47 +0530

    add picom

--- fish/config.fish
+++ fish/config.fish
@@ -8,3 +8,13 @@ alias home "cd ~/"
 # Load fnm (fast node manager)
 set -gx PATH $HOME/.fnm $PATH
 fnm env --use-on-cd | source
+
+# tmux-sessionizer
+if not contains $HOME/.local/scripts $PATH
+    set PATH $PATH $HOME/.local/scripts/
+end
+bind \cf 'tmux-sessionizer'
+bind \eh 'tmux-sessionizer -s 0\n'
+bind \ej 'tmux-sessionizer -s 1\n'
+bind \ek 'tmux-sessionizer -s 2\n'
+bind \el 'tmux-sessionizer -s 3\n'
